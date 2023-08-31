b9fb239d1c174135731494479cb01cdca8eb3f41
SiggiJr <fsiegert1@gmail.com>
2023-08-31 13:35:16 +0200

    Import Export Aufgabe_1_1

---
+++ function.js
@@ -0,0 +1,11 @@
+export const sortArray = (data) => {
+  return data.sort((a, b) => {
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
---
+++ main.js
@@ -0,0 +1,6 @@
+import { arrayNumbers } from "./data.js";
+import { sortArray } from "./function.js";
+import { arrayCars } from "./data.js";
+
+console.log(sortArray(arrayNumbers));
+console.log(sortArray(arrayCars));
